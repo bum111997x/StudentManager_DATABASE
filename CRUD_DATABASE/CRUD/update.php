081016e7a7519314d7e7b03e14d11371a689d0f6
unknown <bum111997x@gmail.comcom>
2019-10-18 08:59:38 +0700

    update anh

--- CRUD_DATABASE/CRUD/update.php
+++ CRUD_DATABASE/CRUD/update.php
@@ -5,14 +5,20 @@ include_once "../Class/DBConnect.php";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $index = $_POST['id'];
-    $image = $_FILES['image']['name'];
-    $target = "../image/" . date('Y-m-d') . basename($image);
-    $student = new User($_GET['name'], $_GET['phone'], $_GET['address'], $target);
     $studentManager = new StudentManager();
     $imageName = $studentManager->showStudentByIndex($index);
-    move_uploaded_file($_FILES['image']['tmp_name'], $target);
-    unlink('../image/'.$imageName->image);
-    $studentManager->update($index, $student);
+    $image = $imageName->image;
+    if ($_FILES['image']['error'] === 4) {
+        $student = new User($_POST['name'], $_POST['phone'], $_POST['address'], $image);
+        $studentManager->update($index, $student);
+    } else if ($_FILES['image']['error'] === 0) {
+        unlink('../image/' . $imageName->image);
+        $image = $_FILES['image']['name'];
+        $target = "../image/" . date('Y-m-d') . basename($image);
+        $student = new User($_POST['name'], $_POST['phone'], $_POST['address'], $target);
+        move_uploaded_file($_FILES['image']['tmp_name'], $target);
+        $studentManager->update($index, $student);
+    }
     header('Location: ../index.php');
 }
 ?>
